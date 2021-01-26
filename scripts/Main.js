dae122e62a30a62661ff765190ae712ca7d74256
Alexis LOURS <alexislours@protonmail.com>
2021-01-26 23:08:42 +0100

    clean: remove console.log

--- scripts/Main.js
+++ scripts/Main.js
@@ -10,8 +10,6 @@ function replace() {
 		let alt = images[i].alt.toLocaleLowerCase();
 		let imgUrl = images[i].src.startsWith("data") ? "" : images[i].src.toLocaleLowerCase();
 		if (new RegExp(terms.join("|")).test(alt) || new RegExp(terms.join("|")).test(imgUrl)) {
-			console.log(alt)
-			console.log(imgUrl)
 			images[i].src = url;
 		}
 	}
